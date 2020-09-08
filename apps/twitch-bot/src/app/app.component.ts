8a387216273778127413003a8352b43e10fe2849
Joshua Van Allen <josh.vanallen@gmail.com>
2020-09-08 18:52:57 -0400

    initial commit

---
+++ apps/twitch-bot/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jvasoftware-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'twitch-bot';
+}
